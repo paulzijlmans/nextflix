648b1a3a146e8ea3e9d3f3260b6c622d51a0244f
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-20 16:20:19 +0200

    Banner Component Button with Font

--- components/banner.js
+++ components/banner.js
@@ -1,3 +1,5 @@
+import styles from './banner.module.css';
+
 export default function Banner(props) {
   const { title, subTitle, imgUrl } = props;
 
@@ -6,11 +8,24 @@ export default function Banner(props) {
   }
 
   return (
-    <div>
-      <h3>{title}</h3>
-      <h3>{subTitle}</h3>
-      <button onClick={handleOnPlay}>Play</button>
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
       <div
+        className={styles.bannerImg}
         style={{
           backgroundImage: `url(${imgUrl})`,
           width: '100%',
