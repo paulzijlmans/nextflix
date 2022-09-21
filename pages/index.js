f7715b863f563c6df23b17bd5f6e76b3108c4094
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-21 23:05:18 +0200

    Youtube API

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import Head from 'next/head';
 
 import Banner from '../components/banner/banner';
-import Card from '../components/card/card';
 import SectionCards from '../components/card/section-cards';
 import NavBar from '../components/nav/nav-bar';
-import { getVideos } from '../lib/videos';
+import { getPopularVideos, getVideos } from '../lib/videos';
 
 import styles from '../styles/Home.module.css';
 
-export default function Home({ disneyVideos }) {
+export default function Home({
+  disneyVideos,
+  travelVideos,
+  productivityVideos,
+  popularVideos,
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,24 +21,35 @@ export default function Home({ disneyVideos }) {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <NavBar username='paul@test.com' />
-      <Banner
-        title='Clifford the red dog'
-        subTitle='a very cute dog'
-        imgUrl='/static/clifford.webp'
-      />
-      <div className={styles.sectionWrapper}>
-        <SectionCards title='Disney' videos={disneyVideos} size='large' />
-        <SectionCards title='Disney' videos={disneyVideos} size='medium' />
+      <div className={styles.main}>
+        <NavBar username='paul@test.com' />
+        <Banner
+          title='Clifford the red dog'
+          subTitle='a very cute dog'
+          imgUrl='/static/clifford.webp'
+        />
+        <div className={styles.sectionWrapper}>
+          <SectionCards title='Disney' videos={disneyVideos} size='large' />
+          <SectionCards title='Travel' videos={travelVideos} size='small' />
+          <SectionCards
+            title='Productivity'
+            videos={productivityVideos}
+            size='medium'
+          />
+          <SectionCards title='Popular' videos={popularVideos} size='small' />
+        </div>
       </div>
     </div>
   );
 }
 
 export async function getServerSideProps() {
-  const disneyVideos = getVideos();
+  const disneyVideos = await getVideos('disney trailer');
+  const productivityVideos = await getVideos('Productivity');
+  const travelVideos = await getVideos('indie music');
+  const popularVideos = await getPopularVideos();
 
   return {
-    props: { disneyVideos },
+    props: { disneyVideos, travelVideos, productivityVideos, popularVideos },
   };
 }
