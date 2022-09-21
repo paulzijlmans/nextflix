dd0ff7f0372187a6c36a97812eec9702dd341c6d
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-21 12:07:22 +0200

    Card Error Handling

--- components/card/card.js
+++ components/card/card.js
@@ -1,9 +1,12 @@
 import Image from 'next/image';
+import { useState } from 'react';
 
 import styles from './card.module.css';
 
 export default function Card(props) {
-  const { imgUrl, size } = props;
+  const { imgUrl = '/static/clifford.webp', size = 'medium' } = props;
+
+  const [imageSource, setImageSource] = useState(imgUrl);
 
   const cardStyleMap = {
     large: styles.largeItem,
@@ -11,13 +14,18 @@ export default function Card(props) {
     small: styles.smallItem,
   };
 
+  function handleOnError() {
+    setImageSource('/static/clifford.webp');
+  }
+
   return (
     <div className={styles.container}>
       <div className={cardStyleMap[size]}>
         <Image
-          src={imgUrl}
+          src={imageSource}
           alt='image'
           layout='fill'
+          onError={handleOnError}
           className={styles.cardImg}
         />
       </div>
